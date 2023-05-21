8e0db802475a533957479019fb9bf65e7190af8f
Adrien Fabre <adrien.fabre@dashlane.com>
2023-05-22 01:07:46 +0200

    Prepare grid generator

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,25 @@
     import { Tabs, Tab, TabList, TabPanel } from 'svelte-tabs';
     import TileEditor from '$lib/components/TileEditor.svelte';
     import ConnectionsEditor from '$lib/components/ConnectionsEditor.svelte';
+    import GridGenerator from '$lib/components/GridGenerator.svelte';
+
+    const tabs = [
+        { title: 'Tiles', component: TileEditor },
+        { title: 'Connections', component: ConnectionsEditor },
+        { title: 'Grid', component: GridGenerator }
+    ];
 </script>
 
 <Tabs>
     <TabList>
-        <Tab>Tiles</Tab>
-        <Tab>Connections</Tab>
+        {#each tabs as tab}
+            <Tab>{tab.title}</Tab>
+        {/each}
     </TabList>
 
-    <TabPanel>
-        <TileEditor />
-    </TabPanel>
-    <TabPanel>
-        <ConnectionsEditor />
-    </TabPanel>
+    {#each tabs as tab}
+        <TabPanel>
+            <svelte:component this={tab.component} />
+        </TabPanel>
+    {/each}
 </Tabs>
