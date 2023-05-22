f751ca342a3f1ded91ecc3b2fe9a3950ebe8085c
Adrien Fabre <adrien.fabre@dashlane.com>
2023-05-22 11:56:17 +0200

    Test CI

--- src/lib/components/GridGenerator.svelte
+++ src/lib/components/GridGenerator.svelte
@@ -1,4 +1,23 @@
 <script lang="ts">
+    import { tiles } from '$lib/tiles';
+    import type { Tile } from './types';
+
+    type Cell =
+        | Tile
+        | {
+              collapsed: false;
+              possibleTiles: number[];
+          };
+
+    const gridSize = 10;
+
+    const nbTiles = $tiles.length;
+    const initialChoices = new Array(nbTiles).fill(0).map((_, id) => id);
+    const grid: Cell[][] = new Array(gridSize).fill(0).map((_) =>
+        new Array(gridSize).fill(0).map((_) => {
+            return { collapsed: false, possibleTiles: [...initialChoices] };
+        })
+    );
 </script>
 
 <h3>Grid generator</h3>
